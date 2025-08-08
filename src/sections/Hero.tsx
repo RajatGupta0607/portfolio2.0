419d581c349885d8cd26487225a7fdd5fb98132e
RajatGupta0607 <rajatgupta0607@gmail.com>
2025-08-08 22:36:15 +0530

    Hero 3D model added, Color fixing needed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import HeroExperience from "../components/HeroModels/HeroExperience";
 import { words } from "../constants";
 
 function Hero() {
@@ -9,6 +10,7 @@ function Hero() {
       </div>
 
       <div className="hero-layout">
+        {/* Left Hero Content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
@@ -43,6 +45,12 @@ function Hero() {
             </Button>
           </div>
         </header>
+        {/* Right Hero Content i.e. 3D Model */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   );
